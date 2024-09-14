aae7cd2946394ca73943f321edf168dc231d48bb
teeniax147 <22dh112354@st.huflit.edu.vn>
2024-09-14 21:24:53 +0700

    First commit

---
+++ QuanLyCuaHangMyPham/Program.cs
@@ -0,0 +1,39 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Thêm CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:5175")  // Địa chỉ ứng dụng React (Vite)
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+app.UseHttpsRedirection();
+app.UseCors("AllowSpecificOrigins");  // Sử dụng cấu hình CORS
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
